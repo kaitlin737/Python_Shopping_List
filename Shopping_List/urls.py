1a4a280f02a5a4386b01a5cc4c9b9ed12218a488
kaitlin737 <kaitlin737@hotmail.com>
2017-11-13 22:45:19 -0700

    added database

--- Shopping_List/urls.py
+++ Shopping_List/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import urls
+from django.conf.urls import url
 from . import views
 
-urlpatterns=[url(r'^$',views.index,name='index),]
+urlpatterns=[url(r'^$',views.index, name='index'),
+]
