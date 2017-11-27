8ba561e487a9a86e1248fa560afa62e5565058e2
candy <perfectpencil1@gmail.com>
2017-11-27 08:32:42 -0700

    Updating settings

--- Shopping_List/admin.py
+++ Shopping_List/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Grocery_list
+from .models import Recipe
+from .models import Ingredients
+from .models import Notes
 
 admin.site.register(Grocery_list)
+admin.site.register(Recipe)
+admin.site.register(Ingredients)
+admin.site.register(Notes)
+
 
 # Register your models here.
