f20cde34d23e0338aa7710d7760f79e085bf2eb9
kaitlin737 <kaitlin737@hotmail.com>
2017-11-29 17:39:48 -0700

    added login

--- Shopping_List/urls.py
+++ Shopping_List/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
+#from mysite.core import views as core_views
 
 urlpatterns=[#url(r'^$',views.index, name='index'),
 url(r'^$',views.Home,name='Home'),
@@ -7,5 +9,8 @@ url(r'^Shopping_List/(?P<pk>\d+)/$', views.bound_form, name='grocerylist_detail'
 url(r'^Shopping_List/saved_lists/$',views.saved_grocery_lists,name='saved_grocery_lists'),
 url(r'^Shopping_List/new/$', views.grocery_new, name='grocery_new'),
 url(r'^Shopping_List/(?P<pk>\d+)/edit/$', views.grocerylist_edit, name='grocerylist_edit'),
-#url(r'^Grocery_List/$',views.Grocery_List,name='Grocery_List'),
+url(r'^signup/$',views.signup,name='signup'),
+url(r'^login/$', auth_views.login, name='login'),
+url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+
 ]
