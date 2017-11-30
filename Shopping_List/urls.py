0d459bb637a66c68f153ffccef24a119adcfc391
candy <perfectpencil1@gmail.com>
2017-11-30 14:26:01 -0700

    saved urls.py

--- Shopping_List/urls.py
+++ Shopping_List/urls.py
@@ -9,14 +9,10 @@ url(r'^Shopping_List/(?P<pk>\d+)/$', views.bound_form, name='grocerylist_detail'
 url(r'^Shopping_List/saved_lists/$',views.saved_grocery_lists,name='saved_grocery_lists'),
 url(r'^Shopping_List/new/$', views.grocery_new, name='grocery_new'),
 url(r'^Shopping_List/(?P<pk>\d+)/edit/$', views.grocerylist_edit, name='grocerylist_edit'),
-<<<<<<< HEAD
-#url(r'^Grocery_List/$',views.Grocery_List,name='Grocery_List'),
-url(r'^Shopping_List/(?P<pk>\d+)/edit/$', views.recipe_add, name='recipe_add'),
-url(r'^Shopping_List/Recipes/$', views,recipe_list, name='recipe_list'),
-=======
+
 url(r'^signup/$',views.signup,name='signup'),
 url(r'^login/$', auth_views.login, name='login'),
 url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 
->>>>>>> master
+
 ]
